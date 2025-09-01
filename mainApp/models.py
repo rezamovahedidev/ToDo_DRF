cab93e86b2ff0da6fa864fa83e5595ab8a6afa5d
Reza <108584275+rezamovaheddi@users.noreply.github.com>
2025-09-01 12:46:51 +0330

    Update models.py

--- mainApp/models.py
+++ mainApp/models.py
@@ -2,10 +2,11 @@ from django.db import models
 
 # Create your models here.
 class ToDo(models.Model):
-	title = models.CharField(max_length=120, blank=True)
+	Title = models.CharField(max_length=120, blank=True)
 	Description = models.TextField(blank=True)
 	Date = models.DateField(blank=False)
-	completed = models.BooleanField(default=False)
+	Completed = models.BooleanField(default=False)
 
 	def __str__(self):
 		return self.title
+
