5400988a9143efc94b29ba79a7bc88bf756b7fbf
Reza <108584275+rezamovaheddi@users.noreply.github.com>
2025-09-01 12:55:36 +0330

    Update serializers.py

--- mainApp/serializers.py
+++ mainApp/serializers.py
@@ -1,7 +1,8 @@
 from rest_framework import  serializers
-from .models import *
+from .models import ToDo
 
 class ToDoSerializer(serializers.ModelSerializer):
 	class Meta :
 		model = ToDo
 		fields = ('id', 'title','Description' , 'Date' ,'completed')
+
