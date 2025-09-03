469f28b3edf4936c0d042d2563ccbfe877951c34
Reza <108584275+rezamovaheddi@users.noreply.github.com>
2025-09-03 12:05:35 +0330

    Update serializers.py

--- mainApp/serializers.py
+++ mainApp/serializers.py
@@ -1,8 +1,13 @@
-from rest_framework import  serializers
+from rest_framework import serializers
 from .models import ToDo
 
 class ToDoSerializer(serializers.ModelSerializer):
-	class Meta :
-		model = ToDo
-		fields = ('id', 'title','Description' , 'Date' ,'completed')
+    class Meta:
+        model = ToDo
+        fields = ('id', 'title', 'description', 'date', 'completed')
+        read_only_fields = ('id', 'date')
 
+    def validate_title(self, value):
+        if ToDo.objects.filter(title=value).exists():
+            raise serializers.ValidationError("A ToDo with this title already exists.")
+        return value
