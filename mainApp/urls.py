f776d8017e0e291ed19ec54409328dcc05d457dc
Reza Movahedi <justunity251@gmail.com>
2025-10-28 23:08:58 +0330

    Update urls.py

--- mainApp/urls.py
+++ mainApp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import *
 from rest_framework import routers
 
+app_name='todo_drf'
 urlpatterns = [
     path('todos/', ListTodo.as_view(), name='todo-list'),
     path('todos/<int:pk>/', DetailTodo.as_view(), name='todo-detail'),
